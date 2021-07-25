939b7670930367f6ae35a22b75b14fa8dc0fdad0
yuki-oshima-revenant <revenant.jumboride@gmail.com>
2021-07-25 20:48:21 +0900

    fix ogp

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,10 +9,12 @@ class MyDocument extends Document {
                     <meta property="og:url" content="https://diary.unronritaro.net" />
                     <meta property="og:type" content="website" />
                     <meta property="og:title" content="Cartesian Theater" />
-                    {/* <meta property="og:image" content="https://source.unsplash.com/peaTniZsUQs" /> */}
+                    <meta property="og:description" content="Yuki Oshima's diary powered by Notion API" />
+                    <meta property="og:image" content="https://diary.unronritaro.net/top.png" />
                     <meta property="og:site_name" content="Cartesian Theater" />
                     <meta name="twitter:card" content="summary" />
-                    {/* <meta name="twitter:image" content="https://source.unsplash.com/peaTniZsUQs" /> */}
+                    <meta name="twitter:image" content="https://diary.unronritaro.net/top.png" />
+                    <meta name="description" content="Yuki Oshima's diary powered by Notion API"></meta>
                 </Head>
                 <body>
                     <Main />
